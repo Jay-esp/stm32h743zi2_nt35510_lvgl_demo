6fbe82eb7251110585d8fbfdfe32455186067f83
Jay-esp <58309071+Jay-esp@users.noreply.github.com>
2023-01-18 01:08:07 +0100

    Update stm32h743_nt35510.ino

--- stm32h743_nt35510.ino
+++ stm32h743_nt35510.ino
@@ -194,8 +194,6 @@ void my_touchpad_read(lv_indev_drv_t * indev, lv_indev_data_t * data)
 static lv_obj_t *tabview;
 
 void setup() {
-  HAL_Init();
-  Stm32_Clock_Init(120, 1, 2, 2);
   delay_init(400);
   LCD_Init();
 
@@ -262,7 +260,7 @@ void loop() {
 
 
   if (millis() - loop_timer >= 5) {
-    lv_tick_inc(1);
+   
     lv_task_handler(); /* let the GUI do its work */
     loop_timer = millis();
   }
@@ -323,79 +321,6 @@ void setupscreens(void) {
 
 // LCD routines start
 
-void Stm32_Clock_Init(u32 plln, u32 pllm, u32 pllp, u32 pllq)
-{
-  HAL_StatusTypeDef ret = HAL_OK;
-  RCC_ClkInitTypeDef RCC_ClkInitStruct;
-  RCC_OscInitTypeDef RCC_OscInitStruct;
-
-  MODIFY_REG(PWR->CR3, PWR_CR3_SCUEN, 0);
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
-
-  while ((PWR->D3CR & (PWR_D3CR_VOSRDY)) != PWR_D3CR_VOSRDY) {}
-
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.HSIState = RCC_HSI_OFF;
-  RCC_OscInitStruct.CSIState = RCC_CSI_OFF;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-
-  RCC_OscInitStruct.PLL.PLLN = plln;
-  RCC_OscInitStruct.PLL.PLLM = pllm;
-  RCC_OscInitStruct.PLL.PLLP = pllp;
-  RCC_OscInitStruct.PLL.PLLQ = pllq;
-
-  RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE;
-  RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_2;
-  ret = HAL_RCC_OscConfig(&RCC_OscInitStruct);
-  // nog debuggen
-  //  if (ret != HAL_OK) while (1);
-
-  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | \
-                                 RCC_CLOCKTYPE_HCLK | \
-                                 RCC_CLOCKTYPE_D1PCLK1 | \
-                                 RCC_CLOCKTYPE_PCLK1 | \
-                                 RCC_CLOCKTYPE_PCLK2 | \
-                                 RCC_CLOCKTYPE_D3PCLK1);
-
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV2;
-  RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV2;
-  RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV4;
-  ret = HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2);
-  if (ret != HAL_OK) while (1);
-
-  __HAL_RCC_CSI_ENABLE() ;
-  __HAL_RCC_SYSCFG_CLK_ENABLE() ;
-  HAL_EnableCompensationCell();
-
-  // test voor adc
-
-  RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
-
-  /** Initializes the peripherals clock
-  */
-  PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_ADC;
-  PeriphClkInitStruct.PLL2.PLL2M = 1;
-  PeriphClkInitStruct.PLL2.PLL2N = 18;
-  PeriphClkInitStruct.PLL2.PLL2P = 2;
-  PeriphClkInitStruct.PLL2.PLL2Q = 2;
-  PeriphClkInitStruct.PLL2.PLL2R = 2;
-  PeriphClkInitStruct.PLL2.PLL2RGE = RCC_PLL2VCIRANGE_3;
-  PeriphClkInitStruct.PLL2.PLL2VCOSEL = RCC_PLL2VCOMEDIUM;
-  PeriphClkInitStruct.PLL2.PLL2FRACN = 6144;
-  PeriphClkInitStruct.AdcClockSelection = RCC_ADCCLKSOURCE_PLL2;
-  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
-  {
-    error1();
-  }
-
-
-}
 
 void delay_init(u16 SYSCLK)
 {
